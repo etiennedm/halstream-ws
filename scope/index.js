b85e9d97f2615498d3e4d74d8238083e5c1a7821
Etienne de Maricourt <etienne.demaricourt@gmail.com>
2018-10-09 12:37:18 +0200

    [scope] Connect to server and show console output

--- scope/index.js
+++ scope/index.js
@@ -1,3 +1,17 @@
 var websocket = require('websocket-stream');
+var d3 = require('d3');
+const { Subject } = require('rxjs');
+const { mergeMap } = require('rxjs/operators');
 
-console.log("Hello, World!");
+var halStream = new Subject();
+
+var halStreamData = halStream.pipe(mergeMap(data => data.toString().split('\n')));
+halStreamData.subscribe(data => {
+    console.log(data);
+});
+
+/* Connect to server */
+var ws = websocket('ws://localhost:8765')
+ws.on('data', (data) => {
+    halStream.next(data);
+});
