0235d48b3b8efd84ae8490734e1855c5efd464bb
Etienne de Maricourt <etienne.demaricourt@gmail.com>
2018-10-09 12:36:57 +0200

    [server] Stream data from halsampler output

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var express = require('express');
 var websocket = require('websocket-stream');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,7 +15,8 @@ var wss = websocket.createServer({
 
 function handle(stream) {
     console.log("handle stream");
-    fs.createReadStream('./halstream.txt').pipe(stream);
+    subprocess = spawn("/home/etienne/dev/linuxcnc/linuxcnc-dev/bin/halsampler", { stdio: ['ignore', 'pipe', 'ignore'] });
+    subprocess.stdout.pipe(stream);
 }
 
 server.listen(process.env.PORT || 8765, () => {
