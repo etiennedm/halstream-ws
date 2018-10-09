c31af9e47e6e2646e5e6a957f94d793b3b9b5665
Etienne de Maricourt <etienne.demaricourt@gmail.com>
2018-10-09 20:08:39 +0200

    [server] Stream data while handling commands received from the client

--- server/index.js
+++ server/index.js
@@ -3,17 +3,34 @@ var express = require('express');
 var websocket = require('websocket-stream');
 const { spawn } = require('child_process');
 
+LINUX_CNC_INSTALL_DIR = '/home/etienne/dev/linuxcnc/linuxcnc-dev/bin';
+
+var hal = require('./hal');
+hal.getPins().then(function(result) {
+    console.log(result);
+});
+
 const app = express();
 const server = http.createServer(app);
 
 var wss = websocket.createServer({
   perMessageDeflate: false,
   server: server,
-  binary: false
 }, handle);
 
+wss.on('connection', (ws, req) => {
+    ws.on('message', (data) => {
+        if (data === 'list pins') {
+            console.log("List pins!");
+            hal.getPins().then(function(result) {
+                ws.send(JSON.stringify(result));
+            });
+        }
+    });
+});
+
 function handle(stream) {
-    subprocess = spawn("/home/etienne/dev/linuxcnc/linuxcnc-dev/bin/halsampler", { stdio: ['ignore', 'pipe', 'ignore'] });
+    subprocess = spawn(`${LINUX_CNC_INSTALL_DIR}/halsampler`, { stdio: ['ignore', 'pipe', 'ignore'] });
     subprocess.stdout.pipe(stream);
 }
 
